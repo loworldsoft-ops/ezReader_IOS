96880608d040822cd15b943a1cbffc8394a7dcc2
“othniel” <othniel.an@gmail.com>
2025-07-19 18:37:39 +0900

    Initial Commit

---
+++ ezReaderTests/ezReaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ezReaderTests.swift
+//  ezReaderTests
+//
+//  Created by an sanghyun on 7/19/25.
+//
+
+import Testing
+@testable import ezReader
+
+struct ezReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
